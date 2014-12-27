b3cc280889e917937bb219732125541e59495c3b
fahad alrahbi <Fahad@fahads-MacBook-Pro.local>
2014-12-27 14:34:32 +0400

    Initial Commit

---
+++ GitHubTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTestApp
+//
+//  Created by fahad alrahbi on 27/12/14.
+//  Copyright (c) 2014 fahad alrahbi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubTestApp
+//
+//  Created by fahad alrahbi on 27/12/14.
+//  Copyright (c) 2014 fahad alrahbi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
